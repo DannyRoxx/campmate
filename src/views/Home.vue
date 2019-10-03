29d1c3152506a34222f5b69428ae50106744b116
Daniell Skourup <dani042z@edu.easj.dk>
2019-10-03 08:51:47 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <list />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import list from '@/components/list.vue'
+
+
+export default {
+  name: 'home',
+  components: {
+    list
+  }
+}
+</script>
